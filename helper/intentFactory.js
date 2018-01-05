b014a653196fbbc1771cdd8ed2df5af434b12346
shekharsingh1987 <shekharsingh_1987@yahoo.com>
2018-01-05 16:30:39 +0530

    Added the basic logic for t002

--- helper/intentFactory.js
+++ helper/intentFactory.js
@@ -11,12 +11,22 @@ var intentsResolver = {
             item2:params['item1'],
             item2_Options:params['option1'],
             pickupOrDelivery:params['Delivery-Pickup-Entity'],
+            address:params['address'],
             paymentMode:params['paymentMode-Entity']
         }
         return JSON.stringify(returnObject);
     },
     t002: function (req) {
-
+        var params = req.body.result.parameters;
+        var returnObject = {
+            item1_quantity:params['number-integer'],
+            item1:params['item'],
+            item1_Options:params['option'],          
+            pickupOrDelivery:params['Delivery-Pickup-Entity'],
+            address:params['address'],
+            paymentMode:params['paymentMode-Entity']
+        }
+        return JSON.stringify(returnObject);
     },
     t003: function (req) {
 
