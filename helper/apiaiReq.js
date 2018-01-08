038c85781dd1c54f18076ea4b5346080074c6376
shekharsingh1987 <shekharsingh_1987@yahoo.com>
2018-01-09 00:37:25 +0530

    Added dialogflow apiAi server

---
+++ helper/apiaiReq.js
@@ -0,0 +1,30 @@
+
+var apiai = require('apiai');
+var apiApp = apiai("00e250db9e5a4c87b9fd703fb3dbbf98");
+
+var getSessionId = function () {
+    var yourDate = new Date();
+    var epochTicks = 621355968000000000;
+    var ticksPerMillisecond = 10000;
+    var yourTicks = epochTicks + (yourDate.getTime() * ticksPerMillisecond);
+    return yourTicks;
+}
+
+
+var apiaiRequest = {
+    process: function (req, cb_result, cb_error) {
+        var request = apiApp.textRequest(req.body.textMessage, {
+            sessionId: getSessionId()
+        });
+
+        request.on('response', function (response) {
+            return cb_result(response.result);
+        });
+
+        request.on('error', function (error) {
+            return cb_error(error);
+        });
+        request.end();
+    }
+};
+module.exports = apiaiRequest; 
