a22b5ddc33434bdd3519ca37e332c2ea8ce81861
shekharsingh1987 <shekharsingh_1987@yahoo.com>
2018-01-09 18:13:32 +0530

    getting the result of speech object with query helper resolved

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();                 // define our app using express
 var bodyParser = require('body-parser');
 var intentRequest = require('./helper/intentReqFactory');
 var apiaiRequest = require('./helper/apiaiReq');
-//var queryHelper = require('./helper/menuDataQueryManager');
+var queryHelper = require('./helper/menuDataQueryManager');
 
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,8 +24,8 @@ router.get('/', function (req, res) {
 router.post('/apiai', function (req, res) {
     var response = apiaiRequest.process(req, function (result) {
         var speechReturned = JSON.parse(result.fulfillment.speech);
-        console.log('log the speech');
-        // console.log(queryHelper.getMenuItem("Pepperoni Cheesy Bread"));
+        console.log('log the speech search implementation');
+        console.log(queryHelper.getMenuItem("Pepperoni Cheesy Bread"));
         res.send(result.fulfillment);
     }, function (error) {
         console.log(error);
--- helper/menuDataQueryManager.js
+++ helper/menuDataQueryManager.js
@@ -1,16 +1,16 @@
-//var jsonQuery = require('json-query');
+var jsonQuery = require('json-query');
 var menuDataRecord = require('./menuData');
 //Get Data from Record of Menu
 var menuData = menuDataRecord.getData();
 // var result = jsonQuery('people[country=NZ]', { data: data }).value;
 
 
-var query = {
-    getMenuItem=function (menuItem) {
-        //var result = jsonQuery('MenuItems[**][*MenuItem=menuItem]',{data:data}).value;
-
+var queryData = {
+    getMenuItem: function (menuItem) {
+        var result = jsonQuery('MenuItems[**][*MenuItem=menuItem]',{data:data}).value;
+        return result;
     }
 
-}
-module.exports = query;
+};
+module.exports = queryData;
 
